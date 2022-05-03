78c1339f3a0572e3a10dae4492f329b85b62751e
Matt Goldman <matt.goldman@outlook.com>
2022-05-04 09:52:17 +1000

    Add project files.

---
+++ MauiClient/Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiClient
+{
+    public static class Constants
+    {
+        public static string B2cScope { get; set; }
+
+        public static string B2cTenantId { get; set; }
+
+        public static string B2cClientId { get; set; }
+
+        public static string B2cPolicy { get; set; }
+
+        public static string AuthorityUri { get; set; }
+    }
+}
---
+++ MauiClient/Services/AuthService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Identity.Client;
+
+namespace MauiClient.Services
+{
+    public class AuthService
+    {
+        private IPublicClientApplication _publicClient;
+
+        public AuthService()
+        {
+            _publicClient = PublicClientApplicationBuilder.Create(Constants.B2cClientId)
+                .WithRedirectUri("goldieb2c://auth")
+                .WithIosKeychainSecurityGroup("com.goldieb2c.mauiclient")
+                .Build();
+        }
+    }
+}
